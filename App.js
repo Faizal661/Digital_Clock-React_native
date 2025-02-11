7ffd2c0b62bcedb71f4c3df1539dd576024423c0
faizal <faisalt661@gmail.com>
2025-02-11 21:39:15 +0530

    feat: digital clock create

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-export default function App() {
+const DigitalClock = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Mohammed Faizal T</Text>
+      <Text style={styles.clockText}>
+        {time.getHours().toString().padStart(2, "0")}:
+        {time.getMinutes().toString().padStart(2, "0")}:
+        {time.getSeconds().toString().padStart(2, "0")}
+      </Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#121212",
+  },
+   heading: {
+    fontSize: 40,
+    fontWeight: "medium",
+    fontStyle:"italic",
+    color: "#ffff",
+  },
+  clockText: {
+    fontSize: 80,
+    fontWeight: "bold",
+    color: "#00ffcc",
   },
 });
+
+export default function App() {
+  return <DigitalClock />;
+}
